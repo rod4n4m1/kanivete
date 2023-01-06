66199e596c66dc605bdafdcd3e751c9adcd1afb1
Rod Anami <rod.anami@br.ibm.com>
2023-01-06 14:33:51 -0300

    Added dockerfile

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# kanivete
+# SRE@Kyndryl
+
+## SRE Public Tools - Kanivete
+
+* Version: `0.1.0`
+* License: `MIT`
---
+++ build/docker-build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export APP_NAME="kanivete"
+export VERSION="0.1.0"
+export USER_NAME="rod4n4m1"
+
+docker login
+docker build . -t $USER_NAME/$APP_NAME:$VERSION
+docker push $USER_NAME/$APP_NAME:$VERSION
