f214ac25d4f2e0a425bb3f026a147ca71603fb5d
Rod Anami <rod.anami@br.ibm.com>
2023-01-06 15:34:01 -0300

    Fixed update script

--- build/update-docker-hub.sh
+++ build/update-docker-hub.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-curl -X PATCH https://cloud.docker.com/v2/repositories/rod4n4m1/kanivete \
+export API_TOKEN=`curl -s -X POST \
+-H "Content-Type: application/json" \
+-d '{"username": "${username}", "password": "${password}"}' \
+https://hub.docker.com/v2/users/login/ | jq -r .token`
+export OVERVIEW=`jq -n --arg msg "$(<../README.md)" '{"registry":"registry-1.docker.io","full_description": $msg }'`
+
+curl -X PATCH https://cloud.docker.com/v2/repositories/${image} \
   -H 'Content-Type: application/json' \
   -H 'Authorization: JWT ${API_TOKEN}' \
   -d '${OVERVIEW}'
